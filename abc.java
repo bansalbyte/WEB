a2c1e2f0b8188e518fea99e9aca4e4c670b637a2
mohitbansal623 <mohit.bansal623@gmail.com>
2017-11-25 14:46:43 +0530

    new commit

---
+++ abc.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class abc {
+  public static void main (String args[]) {
+    System.out.println("defwg");
+    System.out.println("spark-submit --class org.apache.spark.examples.SparkPi /usr/local/spark/spark-2.1.0-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.1.0.jar 1000");
+  }
+}
